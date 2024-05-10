cca4f591064b750bd3d08b689532197c346d11e5
Sodnom <s.zhalsanov@meta.studio>
2024-05-10 21:44:21 +0700

    feat: add migrations for tables and add ignoring of env file

---
+++ database/migrations/2024_05_10_143130_create_images_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('images', function (Blueprint $table) {
+            $table->id();
+            $table->string('title')->nullable();
+            $table->string('alt')->nullable();
+            $table->json('image')->nullable();
+            $table->string('imageable_type');
+            $table->integer('imageable_id')->unsigned()->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('images');
+    }
+};
